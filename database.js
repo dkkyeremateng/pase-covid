fdd7d3fbdfbcbb7ff732c24874d51e63a05b7a4c
Daniel Kofi Kyeremateng <kofwayne305@gmail.com>
2020-05-12 18:23:36 +0000

    first commit

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const uri =
-	'mongodb+srv://wayne:V7jofmk0SC3Iv034@cluster0-360zx.gcp.mongodb.net/PaseCovid?retryWrites=true&w=majority';
+	'mongodb+srv://<username>:<password>@cluster0-360zx.gcp.mongodb.net/PaseCovid?retryWrites=true&w=majority';
 
 mongoose.connect(uri, {
 	useUnifiedTopology: true,
